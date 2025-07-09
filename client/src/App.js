9260db1f956afd08053e9ea5381ffdcc4aace2ad
Tatynaae <tatynaerkinbek@gmail.com>
2025-07-09 12:04:14 +0600

    fix: auth

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
 import Dashboard from './components/Dashboard';
+import RequireAuth from './components/RequireAuth';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/login" element={<LoginForm />} />
       <Route path="/register" element={<RegisterForm />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={
+        <RequireAuth>
+          <Dashboard />
+        </RequireAuth>
+      } />
     </Routes>
   </Router>
 );
